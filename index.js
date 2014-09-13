f4ad70f45d31e24e17421d7c0eef4edde3fb1d44
azu <azuciao@gmail.com>
2014-09-13 17:56:52 +0900

    update bookmarklets

--- index.js
+++ index.js
@@ -5,25 +5,79 @@
  * @license MIT License
  */
 "use strict";
+var mockWindow = window.open('', '_blank');
+(function (scripts, callback, errorback) {
+    if (typeof errorback != 'function') {
+        errorback = function (url) {
+            alert('jsloader load error: ' + url)
+        };
+    }
 
-var formatDates = require("./lib/formatDates");
-var calendarURL = require("./lib/calendarURL");
-(function __main() {
-    var selectText = getSelectingText() || window.prompt("What's date?");
-    var dates = formatDates(selectText);
-    var url = calendarURL({
-        "text": document.title,
-        "details": document.title,
-        "dates": dates
-    });
-    openURL(url);
-})();
+    var cssRegexp = /.css$/;
+    var load = function (url) {
+        if (cssRegexp.test(url)) {
+            var link = document.createElement('link');
+            link.href = url;
+            link.type = 'text/css';
+            link.rel = 'stylesheet';
+            (document.getElementsByTagName('head')[0] || document.body).appendChild(link);
+            if (scripts.length) {
+                load(scripts.shift());
+            } else {
+                callback();
+            }
+        } else {
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.charset = 'utf-8';
+            var current_callback;
+            if (scripts.length) {
+                var u = scripts.shift();
+                current_callback = function () {
+                    load(u)
+                }
+            } else {
+                current_callback = callback;
+            }
+            if (window.ActiveXObject) { // IE
+                script.onreadystatechange = function () {
+                    if (script.readyState == 'complete' || script.readyState == 'loaded') {
+                        current_callback();
+                    }
+                }
+            } else {
+                script.onload = current_callback;
+                script.onerror = function () {
+                    errorback(url)
+                };
+            }
+            script.src = url;
+            document.body.appendChild(script);
+        }
+    };
+    load(scripts.shift());
+})(["https://cdn.rawgit.com/berryboy/chrono/master/chrono.min.js"], function () {
+    var formatDates = require("./lib/formatDates");
+    var calendarURL = require("./lib/calendarURL");
+    (function __main() {
+        var selectText = getSelectingText() || window.prompt("What's date?");
+        var dates = formatDates(selectText);
+        var url = calendarURL({
+            "text": document.title,
+            "details": document.title,
+            "dates": dates
+        });
+        openURL(url);
+    })();
 
-function openURL(url) {
-    window.open(url, "_blank");
-}
+    function openURL(url) {
+        mockWindow.location.href = url;
+        mockWindow.focus();
+    }
 
-function getSelectingText() {
-    return window.getSelection().toString();
-}
+    function getSelectingText() {
+        return window.getSelection().toString();
+    }
 
+
+});
